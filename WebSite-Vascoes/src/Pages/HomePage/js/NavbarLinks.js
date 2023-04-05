1bae796eafd38097f43947d58fa06d11fdf5af4c
henriqueolivgp <henrique.o@ipvc.pt>
2023-04-05 12:04:14 +0100

    ...

--- WebSite-Vascoes/src/Pages/HomePage/js/NavbarLinks.js
+++ WebSite-Vascoes/src/Pages/HomePage/js/NavbarLinks.js
@@ -1,8 +1,8 @@
-// Declaração de variaveis igualando a cada elemento necessario para a função desejada
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-// Esta função terá como função ativar a navbar-links através de um click mostrando assim o resto dos menus
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
